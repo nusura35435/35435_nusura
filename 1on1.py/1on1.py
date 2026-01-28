00065582ccacabb77b54603bb9c4beef7117d3c3
nusura35435 <niyomukizanusura@gmail.com>
2026-01-28 12:30:31 +0000

    Add Python script that prints my name

--- 1on1.py/1on1.py
+++ 1on1.py/1on1.py
@@ -1,1 +1,8 @@
-print("Niyomukiza Nusura")
+print("Niyomukiza Nusura") 
+class student:
+  def __init__(self, name, index_number):
+    self.name = name
+    self.index_number = index_number
+    student1 = student("nusura",35435)
+    print(student1.name)
+    print(student1.index_number)
